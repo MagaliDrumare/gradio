5a96127f1b15ea360019c8b46bb6e09fe9a2f7a9
MD <mag250475@gmail.com>
2025-07-03 18:02:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
-%pip install gradio
-
-import numpy as np
 import gradio as gr
 
-def sepia(input_img):
-    sepia_filter = np.array([
-        [0.393, 0.769, 0.189],
-        [0.349, 0.686, 0.168],
-        [0.272, 0.534, 0.131]
-    ])
-    sepia_img = input_img.dot(sepia_filter.T)
-    sepia_img /= sepia_img.max()
-    return sepia_img
+def tax_calculator(income, marital_status, assets):
+    tax_brackets = [(10, 0), (25, 8), (60, 12), (120, 20), (250, 30)]
+    total_deductible = sum(assets["Cost"])
+    taxable_income = income - total_deductible
 
-demo = gr.Interface(sepia, gr.Image(), "image")
-demo.launch()
+    total_tax = 0
+    for bracket, rate in tax_brackets:
+        if taxable_income > bracket:
+            total_tax += (taxable_income - bracket) * rate / 100
 
-import numpy as np
-import gradio as gr
+    if marital_status == "Married":
+        total_tax *= 0.75
+    elif marital_status == "Divorced":
+        total_tax *= 0.8
 
-def sepia(input_img):
-    sepia_filter = np.array([
-        [0.393, 0.769, 0.189],
-        [0.349, 0.686, 0.168],
-        [0.272, 0.534, 0.131]
-    ])
-    sepia_img = input_img.dot(sepia_filter.T)
-    sepia_img /= sepia_img.max()
-    return sepia_img
+    return round(total_tax)
 
-demo = gr.Interface(sepia, gr.Image(), "image")
+demo = gr.Interface(
+    tax_calculator,
+    [
+        "number",
+        gr.Radio(["Single", "Married", "Divorced"]),
+        gr.Dataframe(
+            headers=["Item", "Cost"],
+            datatype=["str", "number"],
+            label="Assets Purchased this Year",
+        ),
+    ],
+    "number",
+    examples=[
+        [10000, "Married", [["Suit", 5000], ["Laptop", 800], ["Car", 1800]]],
+        [80000, "Single", [["Suit", 800], ["Watch", 1800], ["Car", 800]]],
+    ],
+)
+
 demo.launch()
